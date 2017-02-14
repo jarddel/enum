a3f61fb30127fc92e3c4c315b97c3232c72c96c1
Jarddel Antunes <jarddel@hotmail.com>
2017-02-14 15:46:50 -0300

    [ADD] - Unit tests referring to the base structure of the enumerative classes.

--- tests/EnumTest.php
+++ tests/EnumTest.php
@@ -3,4 +3,31 @@ namespace Robusto\Enum\Tests;
 
 class EnumTest extends \PHPUnit_Framework_TestCase
 {
+    public function testIdentity()
+    {
+        $this->assertEquals(1, LanguageEnum::JAVA);
+    }
+
+    public function testDescription()
+    {
+        $this->assertEquals('Javascript', (string) LanguageEnum::JS());
+    }
+
+    public function testInstance()
+    {
+        $this->assertInstanceOf(LanguageEnum::class, LanguageEnum::PHP());
+    }
+
+    public function testValue()
+    {
+        $this->assertEquals(3, LanguageEnum::PYTHON()->getValue());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidValue()
+    {
+        LanguageEnum::DELPHI();
+    }
 }
